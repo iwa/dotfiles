d276f1d54360cbe3943748bf98c7552a5b464ac8
iwa <iwaongit@gmail.com>
2025-02-26 23:30:57 +0100

    add fish shell config

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,33 @@
+fish_add_path /usr/local/sbin
+fish_add_path /opt/homebrew/bin
+
+set fish_greeting
+
+# Exports
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+export ARCHFLAGS="-arch arm64"
+export TERM=screen-256color
+export EDITOR="nano"
+
+# functions
+function flush-dns
+    sudo killall -HUP mDNSResponder
+    echo "dns cleared successfully"
+end
+
+## google cli
+alias gam-kubic="/Users/iwa/bin/gam-kubic/gam"
+alias gam-carre="/Users/iwa/bin/gam-carre/gam"
+alias gam="/Users/iwa/bin/gam/gam"
+
+# abbrevations
+abbr --add lg lazygit
+abbr --add x exit
+
+# Starship
+export STARSHIP_CONFIG="/Users/iwa/.starship.toml"
+starship init fish | source
